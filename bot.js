4a19083fda712fc10570b9740d3a14912fb482fc
John Barrett <johnjbarrett22@gmail.com>
2019-01-06 11:57:15 -0800

    Post and stream functionality for follow events

--- bot.js
+++ bot.js
@@ -6,28 +6,49 @@ var config = require('./config')
 var T = new Twit(config)
 
 
-//Get
-// var params = {
-//     q: 'warren',
-//     count: 5 
-// };
+// Get
+var params = {
+    q: 'warren',
+    count: 5 
+};
 
-// T.get('search/tweets', params, gotData);
+T.get('search/tweets', params, gotData);
 
-// function gotData(err, data, response){
-//     var tweets = data.statuses;
-//     for(var i = 0; i < tweets.length; i++){
-//         console.log(tweets[i].text);        
-//     }
-// }
+function gotData(err, data, response){
+    var tweets = data.statuses;
+    for(var i = 0; i < tweets.length; i++){
+        console.log(tweets[i].text);        
+    }
+}
 
 //Post
-var tweet = {
-    status: '#Tweeting from node.js'
-};
+function tweetPost(txt){
+    var tweet = {
+        status: 'Testing a 3rd tweet!'
+    };
 
-T.post('statuses/update', tweet, tweeted);
+    T.post('statuses/update', tweet, tweeted);
 
-function tweeted(err, data, response) {
-    console.log(data)
-};
+    function tweeted(err, data, response){
+        if(err){
+            console.log('~Error!~', err)
+        } else {
+            console.log('~Tweet successful!~', data)
+        }  
+    };
+}
+
+//Stream Follow
+var stream = T.stream('user');
+stream.on('follow', followed);
+    
+function followed(eventMsg) {
+    var name = eventMsg.source.name;
+    var screenName = eventMsg.source.screen_name;
+    tweetPost('@' + screenName + ' how long have you liked following robots around?');
+}
+
+
+// setInterval(tweetPost, 1000*20)
+
+tweetPost();
