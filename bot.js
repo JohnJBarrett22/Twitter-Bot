bbc3b860b57ff6001a6a8baa5918000573a03d24
John Barrett <johnjbarrett22@gmail.com>
2019-01-06 03:16:03 -0800

    Added basic get request from Twitter feed

--- bot.js
+++ bot.js
@@ -4,3 +4,18 @@ var Twit = require('twit');
 var config = require('./config')
 
 var T = new Twit(config)
+
+var params = {
+    q: 'warren',
+    count: 5 
+};
+
+T.get('search/tweets', params, gotData);
+
+function gotData(err, data, response){
+    var tweets = data.statuses;
+    for(var i = 0; i < tweets.length; i++){
+        console.log(tweets[i].text);        
+    }
+    // console.log(data);
+}
--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key:         '...',
-    consumer_secret:      '...',
-    access_token:         '...',
-    access_token_secret:  '...',
+    consumer_key:         'OG5FMKLfno16wiDYvarSTsHg0',
+    consumer_secret:      'Znyp1FUFlY41VgpUz0pSuNBipQdrAqIHCFYCZkD4u2Nu2g0NiR',
+    access_token:         '1081735564020174848-DuuiIWfmdd6VGz9p8cUaLqFf3QSI7i',
+    access_token_secret:  '3w5D23CySGpJR22D2ruFgRkeR1aNVe9p2emtemtUxdFLo',
 }
