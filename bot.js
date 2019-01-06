25667f2f009e7fb08c2c8d4a4d571e933b846f2f
John Barrett <johnjbarrett22@gmail.com>
2019-01-06 03:45:23 -0800

    Added posting capability

--- bot.js
+++ bot.js
@@ -5,17 +5,29 @@ var config = require('./config')
 
 var T = new Twit(config)
 
-var params = {
-    q: 'warren',
-    count: 5 
+
+//Get
+// var params = {
+//     q: 'warren',
+//     count: 5 
+// };
+
+// T.get('search/tweets', params, gotData);
+
+// function gotData(err, data, response){
+//     var tweets = data.statuses;
+//     for(var i = 0; i < tweets.length; i++){
+//         console.log(tweets[i].text);        
+//     }
+// }
+
+//Post
+var tweet = {
+    status: '#Tweeting from node.js'
 };
 
-T.get('search/tweets', params, gotData);
+T.post('statuses/update', tweet, tweeted);
 
-function gotData(err, data, response){
-    var tweets = data.statuses;
-    for(var i = 0; i < tweets.length; i++){
-        console.log(tweets[i].text);        
-    }
-    // console.log(data);
-}
+function tweeted(err, data, response) {
+    console.log(data)
+};
